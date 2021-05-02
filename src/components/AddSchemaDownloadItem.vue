e59f33a38cfb51bf74c45c822250a4bc45fd1e21
abdulla bektayev <abdulla.bektayev@gmail.com>
2021-05-02 22:23:58 +0600

    added AddSchemaDownloadItem

---
+++ src/components/AddSchemaDownloadItem.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+  <form @submit.prevent="Generate()">
+    <input v-model="number_of_rows">
+    <button type="submit">Generate</button>
+  </form>
+</div>
+</template>
+
+<script>
+export default {
+  name: "AddSchemaDownloadItem",
+  props: ['DownloadSchemas'],
+  data() {
+    return {
+      number_of_rows: 0
+    }
+  },
+  methods: {
+    Generate() {
+      this.$emit('generate-schema', this.number_of_rows)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/SchemaDownload.vue
+++ src/views/SchemaDownload.vue
@@ -1,23 +1,32 @@
 <template>
-<div class="container">
-  <table class="table table-bordered">
-    <thead>
-    <tr>
-      <th>#</th>
-      <th>Created</th>
-      <th>Status</th>
-      <th>Action</th>
-    </tr>
-    </thead>
-    <SchemaDownloadList
-        v-bind:DownloadSchemas="DownloadSchemas"
-    />
-  </table>
-</div>
+  <div class="container">
+    <div class="container">
+      <AddSchemaDownloadItem
+          v-on:generate-schema="Generate"
+      />
+    </div>
+    <div class="container">
+      <table class="table table-bordered">
+        <thead>
+        <tr>
+          <th>#</th>
+          <th>Created</th>
+          <th>Status</th>
+          <th>Action</th>
+        </tr>
+        </thead>
+        <SchemaDownloadList
+            v-model="DownloadSchemas"
+            v-bind:DownloadSchemas="DownloadSchemas"
+        />
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
 import SchemaDownloadList from "@/components/SchemaDownloadList";
+import AddSchemaDownloadItem from "@/components/AddSchemaDownloadItem";
 export default {
   name: "SchemaDownload",
   data() {
@@ -26,6 +35,7 @@ export default {
     }
   },
   components:{
+    AddSchemaDownloadItem,
     SchemaDownloadList
   },
   mounted() {
@@ -38,6 +48,24 @@ export default {
       })
       .catch(err => console.log(err))
   },
+  methods: {
+    Generate(number_of_rows){
+      fetch('http://0.0.0.0:8000/api/schema/create/csv/', {
+        headers: {
+          'content-type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify({
+          'pk': this.$route.params.id,
+          'row_num': number_of_rows
+        }),
+      })
+          .then(result => result.json())
+          .then(
+              json => this.DownloadSchemas.push(json),
+          )
+    }
+  }
 }
 </script>
 
