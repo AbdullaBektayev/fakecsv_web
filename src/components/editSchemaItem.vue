9d50e2efd751b51a47dff1d3b1914285f1164805
abdulla bektayev <abdulla.bektayev@gmail.com>
2021-05-03 14:17:53 +0600

    added style to editSchema

--- src/components/editSchemaItem.vue
+++ src/components/editSchemaItem.vue
@@ -1,38 +1,54 @@
 <template>
-  <div>
-    <form @submit.prevent="onSubmit(schema.id)">
-      <h3>Name</h3>
-      <br>
-      <input
-          type="text"
-          v-model="schema.Name"
-          placeholder="Name"
-      >
-      <h3>Separator</h3>
-      <br>
+  <div class="container">
+    <div class="container clearfix my-3">
+      <h3 class="float-left">New schema</h3>
+    </div>
+    <form @submit.prevent="onSubmit(schema.id)" class="my-3 clearfix">
+      <div class="row">
+        <div class="col">
+        <div class="form-group col-md-12">
+          <label for="Schema_name" class="float-left">Name</label>
+          <input class="form-control"
+                 id="Schema_name"
+                 type="text"
+                 v-model="schema.Name"
+                 placeholder="Name"
+          >
+      </div>
+      <div class="form-group col-md-12">
+        <label for="Schema_separator" class="float-left">Separator</label>
+        <select v-model="schema.ColumnSeparator" class="form-control" id="Schema_separator">
+          <option
+              v-for="separator in separatorChoices"
+              v-bind:key="separator"
+          >
+            {{ separator }}
+          </option>
+        </select>
+      </div>
 
-      <select v-model="schema.ColumnSeparator">
-        <option
-            v-for="separator in separatorChoices"
-            v-bind:key="separator"
-        >
-          {{ separator }}
-        </option>
-      </select>
+      <div class="form-group col-md-12">
+        <label for="Schema_Char" class="float-left">stringChar</label>
+        <select v-model="schema.StringChar" class="form-control" id="Schema_Char">
+          <option
+              v-for="stringChar in stringCharChoices"
+              v-bind:key="stringChar"
+          >
+            {{ stringChar }}
+          </option>
+        </select>
+      </div>
 
-      <h3>stringChar</h3>
-      <br>
-
-      <select v-model="schema.StringChar">
-        <option
-          v-for="stringChar in stringCharChoices"
-          v-bind:key="stringChar"
-        >
-          {{ stringChar }}
-        </option>
-      </select>
-      <button type="submit">Submit</button>
+      </div>
+        <div class="col clearfix">
+          <button type="submit" class="btn btn-primary float-right">Submit</button>
+        </div>
+      </div>
     </form>
+
+    <div class="container clearfix my-3">
+      <h3 class="float-left">Schema columns</h3>
+    </div>
 
     <columnList
         v-bind:columns="schema.column"
