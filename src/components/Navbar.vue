c1cd3c155ef3058eb21f51adadc4dba75abccd12
abdulla bektayev <abdulla.bektayev@gmail.com>
2021-05-03 08:34:07 +0600

    corrected navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,16 +1,34 @@
 <template>
-  <div class="nav-bar">
-    <nav class="navbar navbar-expand-lg navbar-light bg-white nav-1">
-      <div class="container mw-0 px-3">
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-            <li class="nav-item" v-if="accessToken!=null"><router-link :to = "{ name:'logout' }">Logout</router-link></li>
-          </ul>
-        </div>
+<!--  <div class="nav-bar">-->
+<!--    <nav class="navbar navbar-expand-lg navbar-light bg-white nav-1">-->
+<!--      <div class="container mw-0 px-3">-->
+<!--        <div class="collapse navbar-collapse" id="navbarSupportedContent">-->
+<!--          <ul class="navbar-nav mr-auto">-->
+<!--            <li class="nav-item" v-if="accessToken!=null"><router-link :to = "{ name:'logout' }">Logout</router-link></li>-->
+<!--          </ul>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--    </nav>-->
+<!--  </div>-->
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">FakeCSV</a>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link :to = "{ name:'schema_list' }">Schemas</router-link>
+        </li>
+      </ul>
+      <div class="navar-right">
+        <span class="navbar-text mx-3">
+          Hello, {{ this.$store.state.username }}
+        </span>
+        <span class="navbar-text mx-3">
+            <p><router-link :to = "{ name:'logout' }">Logout</router-link></p>
+        </span>
       </div>
-    </nav>
-  </div>
+    </div>
+  </nav>
 </template>
 
 <script>
--- src/store.js
+++ src/store.js
@@ -7,19 +7,24 @@ export default new Vuex.Store({
     state: {
         accessToken: localStorage.getItem('accessToken'),
         refreshToken: localStorage.getItem('refreshToken'),
+        username: localStorage.getItem('username')
     },
     mutations: {
-        updateStorage (state, { access, refresh }) {
+        updateStorage (state, { access, refresh, username }) {
             state.accessToken = access
             state.refreshToken = refresh
+            state.username = username
             localStorage.setItem('accessToken', access)
             localStorage.setItem('refreshToken', refresh)
+            localStorage.setItem('username', username)
         },
         destroyToken (state) {
             state.accessToken = null
             state.refreshToken = null
+            state.username = null
             localStorage.setItem('accessToken', null)
             localStorage.setItem('refreshToken', null)
+            localStorage.setItem('username', null)
         }
     },
     getters: {
@@ -37,10 +42,14 @@ export default new Vuex.Store({
             return new Promise((resolve, reject) => {
                 getAPI.post('/api-token/', {
                     username: usercredentials.username,
-                    password: usercredentials.password
+                    password: usercredentials.password,
                 })
                     .then(response => {
-                        context.commit('updateStorage', { access: response.data.access, refresh: response.data.refresh })
+                        context.commit('updateStorage', {
+                            access: response.data.access,
+                            refresh: response.data.refresh,
+                            username: usercredentials.username,
+                        })
                         resolve()
                     })
                     .catch(err => {
