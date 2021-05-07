35cbb82b0b5f0b70950c5217875d599fbc87e6ca
abdulla bektayev <abdulla.bektayev@gmail.com>
2021-05-07 19:28:01 +0600

    commit for deploying

---
+++ dist/js/chunk-2d215fa4.bd8b6da8.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d215fa4"],{c100:function(t,n,e){"use strict";e.r(n);var s=function(){var t=this,n=t.$createElement,e=t._self._c||n;return e("div",{staticClass:"log"})},u=[],c={created:function(){var t=this;this.$store.dispatch("userLogout").then((function(){t.$router.push({name:"login"})}))}},o=c,a=e("2877"),i=Object(a["a"])(o,s,u,!1,null,null,null);n["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d215fa4.bd8b6da8.js.map
